d9a31552b79f3c3f2a746c585caf880cfacd1c0a
destroylord <masapin68@gmail.com>
2021-01-02 19:49:12 +0700

    state hooks

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,26 @@
+import { useState } from 'react';
+
 const Home = () => {
+
+
+    // let name = 'Dafrin';
+    const [name, setName] = useState('Dafrin');
+    const [age, setAge] = useState(19);
+
+    const handleClick = () => {
+        setName('FAHMI DAFRIN MAULANA');
+        setAge(20);
+    }
+
+    const handleClickAgain = (name) => {
+        console.log('hello '+ name);
+    }
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <p>{name} is {age} years old</p>
+            <button onClick={handleClick}>Click me</button>
+            <button onClick={ () => handleClickAgain('dafrin') }>Click me Again</button>
         </div>
      );
 }
