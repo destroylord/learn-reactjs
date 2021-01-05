bc4af899a6b6a2d2e042d3c2c8ac2694a7a13aba
destroylord <masapin68@gmail.com>
2021-01-05 20:02:27 +0700

    outputing list

--- src/Home.js
+++ src/Home.js
@@ -6,6 +6,11 @@ const Home = () => {
     // let name = 'Dafrin';
     const [name, setName] = useState('Dafrin');
     const [age, setAge] = useState(19);
+    const [blogs, setBlogs] = useState([
+        {title : 'My new website', body : 'lorem ipsum..', author : 'dafrin', id : 1},
+        {title : 'My website', body : 'lorem ipsum..', author : 'rizal', id : 2},
+        {title : 'Welcome to my website', body : 'lorem ipsum..', author : 'didik', id : 3}
+    ]);
 
     const handleClick = () => {
         setName('FAHMI DAFRIN MAULANA');
@@ -21,6 +26,12 @@ const Home = () => {
             <p>{name} is {age} years old</p>
             <button onClick={handleClick}>Click me</button>
             <button onClick={ () => handleClickAgain('dafrin') }>Click me Again</button>
+                {blogs.map((blog) => (
+                    <div className="blog-preview" key={blog.id} >
+                        <h2>{blog.title}</h2>
+                        <p>Write by {blog.author}</p>
+                    </div>
+                ))}
         </div>
      );
 }
