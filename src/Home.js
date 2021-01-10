07abd00c8c590afab1665bc4877cae7a80a3f2f2
destroylord <masapin68@gmail.com>
2021-01-10 23:17:44 +0700

    useEffect & useEffect Dependencies

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
@@ -13,10 +13,18 @@ const Home = () => {
         {title : 'Welcome to my website', body : 'lorem ipsum..', author : 'didik', id : 3}
     ]);
 
+    
     const handleClick = () => {
         setName('FAHMI DAFRIN MAULANA');
         setAge(20);
     }
+    
+    const [name, setName] = useState('Mario');
+    // useEffect hook
+    useEffect(() => {
+        console.log('use effect ran');
+        console.log(name);
+    }, [name]);
 
     const handleDelete = (id) => {
         const newBlogs = blogs.filter(blog => blog.id !== id);
@@ -31,9 +39,12 @@ const Home = () => {
             <h2>Homepage</h2>
             <p>{name} is {age} years old</p>
             <button onClick={handleClick}>Click me</button>
+
             <button onClick={ () => handleClickAgain('dafrin') }>Click me Again</button>
 
                 <BlogList blogs={blogs} title="All blogs!!" handleDelete={handleDelete}></BlogList>
+                <button onClick={() => setName('luigi')}>Change name</button>
+                <p>{name}</p>
         </div>
      );
 }
